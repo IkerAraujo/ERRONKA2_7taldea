9eb02cb059dc91da2a100e38c3d4210106faeee1
Iker-Araujo-Dominguez <you@example.com>
2026-01-25 16:59:54 +0100

    3ENT7_ARIK bilatzailea

---
+++ weberronka/navbar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+         <div class="nav">
+            <a href="SARRERA.php">
+               <img src="img/TechnoPlus.png" height="125" width="125" alt="SecondsAgo logo"></a>
+            <form action="" class="bilatzailea">
+              <input type="text" name="Bilatu...">
+            </form>
+               <a href="KATALOGOA.php">KATALOGOA</a>
+            <a href="SARRERA.php">SARRERA</a>
+              <a href="HASI SAIOA.php">HASI SAIOA</a>
+            <a href="IZENA EMAN.php">FORMULARIOA</a>
+            <img src="img/Reino Unido.jpg"  class="Ingles"alt="English logo">
+            <img src="img/saskia.png" alt="" height="12%" width="5%" class="saskia">
+            <div class="zenbaki-saskia">
+                <span id="saskia"> 0 </span>
+            </div>
+        </div>
+        </nav>
+</body>
+</html>
